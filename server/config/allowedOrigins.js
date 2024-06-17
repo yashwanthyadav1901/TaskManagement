8bdf9d00c0c731a0de19df710af700eca1f6dc98
yashwanthyadav1901 <yashwanthyadav481@gmail.com>
2024-06-17 10:37:36 +0530

    basic

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://task-management-neon-one.vercel.app/"];
+const allowedOrigins = [
+  "http://localhost:3000/",
+  "https://task-management-neon-one.vercel.app/",
+];
 
 module.exports = allowedOrigins;
